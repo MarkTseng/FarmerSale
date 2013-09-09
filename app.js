a50652bf96c96d0021d023abe2fb8af3f653689a
mark <mark@mark-debian>
2013-09-10 01:37:40 +0800

    [A] initial web framework

---
+++ app.js
@@ -0,0 +1,76 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+//  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var settings = require('./settings');
+//var MongoStore = require('connect-mongodb');
+var MongoStore = require('connect-mongo')(express);
+var flash = require('connect-flash');
+
+
+var app = express();
+
+// use ejs-locals for all ejs templates:
+app.engine('ejs', require('ejs-locals'));
+
+// all environments
+app.set('port', process.env.PORT || 80);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('my secret'));
+app.use(express.session({
+  key: 'session_id',
+  cookie: {maxAge: 60000},
+  secret: settings.cookieSecret,
+  store: new MongoStore({
+    db: settings.db
+  })
+}));
+
+//connect-flash
+app.use(flash());
+
+//Express 3.x
+app.use(function(req, res, next){
+  res.locals.user = req.session.user;
+  var err = req.flash('error');
+  if(err.length)
+    res.locals.error = err;
+  else
+    res.locals.error = null;
+  var succ = req.flash('success');
+  if(succ.length)
+    res.locals.success = succ;
+  else
+    res.locals.success = null;
+  next();
+});
+
+
+app.use(app.router);
+//app.use(express.router(routes));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+routes(app);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ settings.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  cookieSecret: 'carpoolscrect',
+  db: 'carpool',
+  host: 'localhost',
+};
